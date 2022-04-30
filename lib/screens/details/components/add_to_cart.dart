3071428e8cc02ec1a3010c3cf2f116ac846ec68d
lenguyengiabao <lenguyengiabao46@gmail.com>
2022-04-30 16:01:09 +0700

    cart function

--- lib/screens/details/components/add_to_cart.dart
+++ lib/screens/details/components/add_to_cart.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
 import 'package:strarry_flutter/models/Product.dart';
+import 'dart:convert';
 
 import '../../../constants.dart';
 
@@ -11,6 +13,37 @@ class AddToCart extends StatelessWidget {
   }) : super(key: key);
 
   final Product product;
+
+  Future<bool> update_Cart(String id_product) async {
+    String id_account = '1'; // NEED MODIFY
+    // String id_product = product.id.toString();
+    print(id_product);
+    String amount_product = '1'; // NEED TO MODIFY
+
+    var request =
+        http.MultipartRequest('POST', Uri.parse(backend + 'cart/update/'));
+    request.fields.addAll({
+      'id_account': id_account,
+      'id_product': id_product,
+      'amount_product': amount_product
+    });
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      var responseAwait = await response.stream.bytesToString();
+      var responseJson = jsonDecode(responseAwait);
+      var updateSuccess = responseJson["success"];
+      if (updateSuccess == 'true') {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      print(response.reasonPhrase);
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +66,12 @@ class AddToCart extends StatelessWidget {
                 "assets/icons/add_to_cart.svg",
                 color: product.color,
               ),
-              onPressed: () {},
+              onPressed: () async {
+                var isUpdateSuccess = await update_Cart(product.id.toString());
+                if (isUpdateSuccess == true) {
+                  _showToast(context, true);
+                }
+              },
             ),
           ),
           Expanded(
@@ -45,7 +83,7 @@ class AddToCart extends StatelessWidget {
                 color: product.color,
                 onPressed: () {},
                 child: Text(
-                  "Buy  Now".toUpperCase(),
+                  "Buy Now".toUpperCase(),
                   style: TextStyle(
                     fontSize: 17,
                     fontWeight: FontWeight.bold,
@@ -58,5 +96,22 @@ class AddToCart extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _showToast(BuildContext context, bool isSuccess) {
+    final scaffold = ScaffoldMessenger.of(context);
+    if (isSuccess == true) {
+      scaffold.showSnackBar(
+        SnackBar(
+          content: const Text('Added to cart'),
+        ),
+      );
+    } else {
+      scaffold.showSnackBar(
+      SnackBar(
+        content: const Text('Add failed'),
+      ),
+    );
+    }
   }
 }
--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -40,11 +40,11 @@ class Body extends StatelessWidget {
                   ),
                   child: Column(
                     children: <Widget>[
-                      ColorAndSize(product: product),
+                      // ColorAndSize(product: product),
                       SizedBox(height: kDefaultPaddin / 2),
                       Description(product: product),
                       SizedBox(height: kDefaultPaddin / 2),
-                      CounterWithFavBtn(),
+                      // CounterWithFavBtn(),
                       SizedBox(height: kDefaultPaddin / 2),
                       AddToCart(product: product)
                     ],
